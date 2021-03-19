bf323891fb5619ceb0233aa8d0b44b4083d297b4
Aleksa <jaksicaleksa77@gmail.com>
2021-03-19 15:42:12 +0100

    Add lodging

---
+++ TuristickaAgencija/TuristickaAgencija/Data/Migrations/20210319134923_DodavanjeSmestaja.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TuristickaAgencija.Data.Migrations
+{
+    public partial class DodavanjeSmestaja : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Smestaji",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BrojKreveta = table.Column<int>(type: "int", nullable: false),
+                    Tip = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cena = table.Column<float>(type: "real", nullable: false),
+                    AranzmanId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Smestaji", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Smestaji_Aranzmani_AranzmanId",
+                        column: x => x.AranzmanId,
+                        principalTable: "Aranzmani",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Smestaji_AranzmanId",
+                table: "Smestaji",
+                column: "AranzmanId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Smestaji");
+        }
+    }
+}
---
+++ TuristickaAgencija/TuristickaAgencija/Models/Smestaj.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TuristickaAgencija.Models
+{
+    public class Smestaj : BaseModel
+    {
+        [Required]
+        [Range(1,10)]
+        public int BrojKreveta { get; set; }
+
+        [Required]
+        public string Tip { get; set; }
+
+        [Required]
+        public float Cena { get; set; }
+
+        [Display(Name ="Izaberi aranzman")]
+        public Guid AranzmanId { get; set; }
+
+        public Aranzman Aranzman { get; set; }
+    }
+}
